490d731520aff53200256ffddf896fb5d4ec6dff
Turbobot <turbobot@vercel.com>
2024-06-28 19:43:07 +0800

    feat(create-turbo): apply official-starter transform

--- apps/api/src/utils.ts
+++
@@ -1,9 +0,0 @@
-import type { AddContentTypeParser, FastifyInstance } from 'fastify';
-
-export const setContentTypeParser = (
-  app: FastifyInstance,
-  ...[contentType, opts, parser]: Parameters<AddContentTypeParser>
-) => {
-  app.removeContentTypeParser(contentType);
-  app.addContentTypeParser(contentType, opts, parser);
-};
--- packages/utilities/.eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ['custom/node'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: true,
-  },
-};
