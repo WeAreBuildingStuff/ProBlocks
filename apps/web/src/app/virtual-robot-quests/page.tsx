83f778af16feb5e969896e7a0c9f41f4016ad8c3
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2024-07-31 00:37:07 +0800

    [Fix] React must be in scope

--- apps/web/src/app/virtual-robot-quests/page.tsx
+++ apps/web/src/app/virtual-robot-quests/page.tsx
@@ -1,9 +1,10 @@
-import { QuestListScroll } from "../../components/questListScroll"
+import { QuestListScroll } from '../../components/questListScroll';
+import React from 'react';
 
 export default function Page(): JSX.Element {
   return (
-    <main className="w-screen flex bg-neutral-100 px-24 pt-12">
-      <div className="h-full w-2/5 flex flex-col items-center">
+    <main className='w-screen flex bg-neutral-100 px-24 pt-12'>
+      <div className='h-full w-2/5 flex flex-col items-center'>
         <QuestListScroll />
       </div>
     </main>
