e3954965a13579710add66a02653669ae0fc4a52
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-08-04 02:30:19 +0800

    refactor: remove unecessary folder / pages

--- apps/docs/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Page(): JSX.Element {
-  return (
-    <main className="w-screen h-screen flex flex-col items-center justify-between min-h-screen p-24">
-
-    </main>
-  );
-}
