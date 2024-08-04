9549d3f20b906528ce898a78d47c258cd8c45a87
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2024-08-04 18:14:15 +0800

    [Add] getMoveCommands

---
+++ apps/backend/src/utils/gemini/getMoveCommands.ts
@@ -0,0 +1,34 @@
+import { googleAI } from '@genkit-ai/googleai';
+import { dotprompt, prompt } from '@genkit-ai/dotprompt';
+import { configureGenkit } from '@genkit-ai/core';
+// import { defineSchema } from '@genkit-ai/core';
+// import z from 'zod';
+
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+configureGenkit({
+  plugins: [
+    googleAI({ apiKey: process.env.GOOGLE_GENAI_API_KEY, apiVersion: 'v1beta' }),
+    dotprompt()
+  ],
+  logLevel: 'debug',
+  enableTracingAndMetrics: true
+});
+
+export async function getMoveCommands(instruction: string) {
+  const newPrompt = await prompt('move_command');
+
+  const result = await newPrompt.generate({
+    input: {
+      instructions: instruction
+    }
+  });
+
+  const output = result.text();
+
+  console.log(output);
+
+  return output;
+}
